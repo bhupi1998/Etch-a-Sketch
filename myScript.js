eda6fa1c361dd9bda6920d387e969f032e86e79b
bhupinder <bhupi98.bs@gmail.com>
2021-10-16 16:50:08 -0700

    Created basic functions to create a grid

--- myScript.js
+++ myScript.js
@@ -1,20 +1,21 @@
-function gridMaker(size){
-    const rowDiv = document.createElement('div');
-    const colDiv = document.createElement('div');
+function rowMaker(size){
     for(let i=0;i<size;i++){
-        rowDiv.setAttribute('id',`row${i}`);
-        rowDivCurr=document.querySelector(`row${i}`);
-        console.log(rowDivCurr);
-        gridElement.appendChild(rowDivCurr);
-        for(let j=0;j<size;i++){
-            colDiv.setAttribute('id',`col${j}`);
-            colDivCurr=document.querySelector(`col${j}`);
-            console.log(colDivCurr);
-            rowDivCurr.appendChild(colDivCurr);
-        }
+        gridElement.innerHTML+=`<div id='row${i}' class='row'>HEllo</div>`;
+        colMaker(size,i);
     }
 
 }
-gridElement=document.querySelector("#gridContainer");
+function colMaker(size,rowIndex){
+    const rowElement=document.querySelector(`#row${rowIndex}`);
+    for(let j=0;j<size;j++){
+        rowElement.innerHTML+=`<div id='col${j}'>HEllo</div>`;
+    }
+}
+function gridMaker(size){
+    rowMaker(size);
+}
+
+const gridElement=document.querySelector("#gridContainer");
 console.log(gridElement);
-gridMaker(4);
+gridMaker(9);
+
